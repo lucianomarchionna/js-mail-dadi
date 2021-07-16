731f76279e250a01f8fd60430239f27fda5f666c
Luciano <lucco6823@gmail.com>
2021-07-16 16:40:30 +0200

    Esercizio mail completo

---
+++ Gruppo 1/mail.js
@@ -0,0 +1,23 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+var mail = prompt("Inserisci la tua mail: ");
+console.log(mail);
+
+var mail_access = ["a@boolean.it", "b@boolean.it", "c@boolean.it", "d@boolean.it", "e@boolean.it", "f@boolean.it", "g@boolean.it"];
+var controllo = false; 
+
+for(var i = 0; i < mail_access.length; i++){
+    if(mail == mail_access[i]){
+        controllo = true;
+    }
+}
+
+if(controllo){
+    console.log("Accesso Consentito");
+    document.getElementById("success").innerHTML = "Ciao, la mail inserita ("+ mail + ") possiede l'accesso al sito!";
+}
+else{
+    document.getElementById("declined").innerHTML = "Ciao, la mail inserita ("+ mail + ") purtroppo non possiede l'accesso al sito! La preghiamo di riprovare.";
+}
